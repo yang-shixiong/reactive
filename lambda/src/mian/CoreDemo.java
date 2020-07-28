c5c771f9ebbae7cc6aa47eff977db170fd73fade
Mark <mark.yang@imagego.com>
2020-07-28 14:07:52 +0800

    :delete:delete unuser file

--- lambda/src/mian/CoreDemo.java
+++ lambda/src/mian/CoreDemo.java
@@ -1,17 +1,3 @@
-/**
- * Copyright(c) JingHong Technology Co., Ltd.
- * All Rights Reserved.
- * <p>
- * This software is the confidential and proprietary information of JingHong
- * Technology Co.,Ltd. ("Confidential Information"). You shall not disclose
- * such Confidential Information and shall use it only in accordance with the
- * terms of the license agreement you entered into with JingHong.
- * For more information about JingHong, welcome to https://www.imagego.com
- * <p>
- * Revision History:
- * Date         Version    Name       Description
- * 2020/6/2    1.0        mark       File Creation
- */
 package mian;
 
 import java.util.function.Consumer;
